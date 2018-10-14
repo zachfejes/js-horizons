c45412cd27b4cdb64cfe74172f2fec600195ce34
Zach <zach@secondcloset.com>
2018-10-14 01:12:16 -0400

    Parser can now structure multiple body response as JSON

--- src/parseSearchData.js
+++ src/parseSearchData.js
@@ -8,12 +8,40 @@ module.exports = function parseSearchData(searchData) {
         throw TYPE_ERROR_STRING;
     }
 
+    let dataArray = [];
+    let parsedData = [];
+    let i = 0;
+
     if(searchData.match(REGEX_SINGLE_BODY_FOUND)) {
         console.log("We have uniquely specified a body");
     }
-
-    if(searchData.match(REGEX_MULTIPLE_BODIES_FOUND)) {
+    else if(searchData.match(REGEX_MULTIPLE_BODIES_FOUND)) {
         console.log(`We have found multiple bodies that match the query`);
-    }
 
+        dataArray = searchData.split(/[\*]{2,}/)[1].split(/[\n|\r\n]/); //extract table data
+        dataArray = dataArray.filter((line) => line.trim() !== "");
+
+        let startIndex = dataArray.findIndex((line) => line.match(/[\-]{2,}/));
+        startIndex++;
+        let endIndex = dataArray.findIndex((line) => line.includes("Number of matches"));
+        dataArray = dataArray.slice(startIndex, endIndex);
+        
+        let brokenString = [];
+        for(i = 0; i < dataArray.length; i++) {
+            dataArray[i] = dataArray[i].trim();
+            brokenString = dataArray[i].split(/(?<!\s)[\s]{2}|[\s]{2}(?!\s)/g);
+
+            parsedData.push({
+                id: brokenString[0],
+                name: brokenString[1],
+                designation: brokenString[2] && brokenString[2].trim() !== "" ? brokenString[2] : undefined,
+                iau: brokenString[3] && brokenString[3].trim() !== "" ? brokenString[3] : undefined
+            });
+        }
+
+        return(parsedData);
+    }
+    else {
+        console.log("The data is of an unknown structure, and could not be parsed");
+    }
 }
