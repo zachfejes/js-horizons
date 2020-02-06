95becd936bd64487ae6f81c857ccac83ad69eaae
Zach <zach@secondcloset.com>
2020-02-05 23:04:09 -0500

    Test Data output has been clarified

--- sandbox/index.js
+++ sandbox/index.js
@@ -2,8 +2,10 @@ const Horizons = require("../index");
 
 const horizons = new Horizons();
 horizons.initialize((error, response) => {
-    horizons.search("99942", (error, response) => {
+    horizons.search("599", (error, response) => {
+        console.log("***  DATA RETRIEVED  ***");
         console.log(response);
+        console.log("***  \DATA RETRIEVED  ***");
         horizons.close();
     });
 });
